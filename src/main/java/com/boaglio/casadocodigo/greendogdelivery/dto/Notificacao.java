9baeada5260071c8f534ebe76f4aed4478aabe4e
Fernando Boaglio <boaglio@gmail.com>
2017-03-05 20:48:37 +0000

    crud

--- src/main/java/com/boaglio/casadocodigo/greendogdelivery/dto/Notificacao.java
+++
@@ -1,8 +0,0 @@
-package com.boaglio.casadocodigo.greendogdelivery.dto;
-
-
-public interface Notificacao {
-
-	boolean envioAtivo();
- 
-}
--- src/main/java/com/boaglio/casadocodigo/greendogdelivery/repository/ItemRepository.java
+++ src/main/java/com/boaglio/casadocodigo/greendogdelivery/repository/ItemRepository.java
@@ -1,16 +1,12 @@
 package com.boaglio.casadocodigo.greendogdelivery.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.boaglio.casadocodigo.greendogdelivery.domain.Item;
-import com.boaglio.casadocodigo.greendogdelivery.domain.Pedido;
 
-//@Repository
-@RepositoryRestResource(collectionResourceRel = "itens",path = "itens")
+@Repository
+//@RepositoryRestResource(collectionResourceRel = "itens",path = "itens")
 public interface ItemRepository extends JpaRepository<Item, Long> {
  
 }
