43207aabb178b594f456ccbb47c5ca094c95dffd
Fernando Boaglio <boaglio@gmail.com>
2017-04-12 00:35:33 +0000

    cloud

---
+++ sobe-sistema-na-porta-8081.sh
@@ -0,0 +1,5 @@
+
+cd target
+
+java -jar  -Dserver.port=8081 green-dog-delivery-1.0.0-SNAPSHOT.jar
+
---
+++ sobe-sistema-na-porta-8083.sh
@@ -0,0 +1,5 @@
+
+cd target
+
+java -jar  -Dserver.port=8083 green-dog-delivery-1.0.0-SNAPSHOT.jar
+
