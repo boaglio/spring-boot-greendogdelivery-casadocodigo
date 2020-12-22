df91be947c4af8d2d2230dfb577473e5779d0ad2
boaglio <boaglio@gmail.com>
2020-12-22 03:01:53 -0300

    upgrade para 2.4

--- src/main/java/com/boaglio/casadocodigo/greendogdelivery/config/SpringDataRestCustomization.java
+++ src/main/java/com/boaglio/casadocodigo/greendogdelivery/config/SpringDataRestCustomization.java
@@ -1,19 +1,20 @@
 package com.boaglio.casadocodigo.greendogdelivery.config;
 
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.boaglio.casadocodigo.greendogdelivery.domain.Item;
 import com.boaglio.casadocodigo.greendogdelivery.repository.ClienteRepository;
 
 @Component
 public class SpringDataRestCustomization 
-extends RepositoryRestConfigurerAdapter 
+implements RepositoryRestConfigurer 
 {
 
 	@Override
-	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		config.exposeIdsFor(Item.class,ClienteRepository.class);
 	}
 }
