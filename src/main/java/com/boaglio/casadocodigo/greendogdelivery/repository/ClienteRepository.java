0f335e344d721803c56230ed793f05e421162f29
boaglio <boaglio@gmail.com>
2020-12-29 16:36:39 -0300

    limpando arquivos

--- src/main/java/com/boaglio/casadocodigo/greendogdelivery/repository/ClienteRepository.java
+++ src/main/java/com/boaglio/casadocodigo/greendogdelivery/repository/ClienteRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.boaglio.casadocodigo.greendogdelivery.domain.Cliente;
