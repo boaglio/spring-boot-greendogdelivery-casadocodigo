d5a725f4b6fc54460a423b2f08c0f4d6d58d99d1
boaglio <boaglio@gmail.com>
2020-12-31 16:25:34 -0300

    upgrade para 2.4.1

---
+++ src/main/java/com/boaglio/casadocodigo/greendogdelivery/config/HttpTraceActuatorConfiguration.java
@@ -0,0 +1,16 @@
+package com.boaglio.casadocodigo.greendogdelivery.config;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpTraceActuatorConfiguration {
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+
+}
